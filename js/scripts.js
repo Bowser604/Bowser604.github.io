5cc4e46324c537f5a8dc090d701c29d21c4f8f17
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-10-10 09:34:09 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,8 @@
-alert('Hello world');
 
-let pokemonlist = (function () {
-    let repository = [
-        {name: 'Charizard', height: 17, types: ['Fire'],
-         abilities: ['blaze', 'solar-power']},
-        {name: 'Pikachu', height: 4, types: ['Electric'], 
-         abilities: ['static', 'lighting-rod']},
-        {name: 'Abra', height: 9, types: ['Psychic'], 
-         abilities: ['synchronize', 'inner-focus']}
-    ];
+alert("Hello, World");
 
-for (let i = 0; i < pokemonlist.length; i++) {
-  if (pokemonlist[i].height > 1)
-   {document.write(pokemonlist[i].name + "  " + "(height:"+ "  " +  pokemonlist[i].height + " ) " + "<br>");
-document.write(pokemonlist[i].name + "  " + "(height:"+ "  " +  pokemonlist[i].height + " ) " + "<br>");}
-}
-  
+
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
