4dd757cb9982ba4ae8a04edc5f35fbab611bc503
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-10-12 12:06:58 -0700

    Update scripts.js
    Pokemon list with array added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-
-alert("Hello, World");
-
-const favoriteFood = 'ramen'; 
-document.write(favoriteFood); 
-
+let pokemonlist = (function () {
+    let repository = [
+        {name: 'Charizard', height: 17, types: ['Fire'],
+         abilities: ['blaze', 'solar-power']},
+        {name: 'Pikachu', height: 4, types: ['Electric'], 
+         abilities: ['static', 'lighting-rod']},
+        {name: 'Abra', height: 9, types: ['Psychic'], 
+         abilities: ['synchronize', 'inner-focus']}
+    ];
