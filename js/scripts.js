bced56a6fd42204c6efb074f5fa0159df1d7c077
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-10-13 20:04:25 -0700

    Update scripts.js
    Removed function line

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
-let pokemonlist = (function () {
-    let repository = [
-        {name: 'Charizard', height: 17, types: ['Fire'],
-         abilities: ['blaze', 'solar-power']},
-        {name: 'Pikachu', height: 4, types: ['Electric'], 
-         abilities: ['static', 'lighting-rod']},
-        {name: 'Abra', height: 9, types: ['Psychic'], 
-         abilities: ['synchronize', 'inner-focus']}
-    ];
+let pokemonList = [{name: 'Charizard', height: 17, types: ['Fire'], 
+                    abilities: ['blaze', 'solar-power']},
+                   {name: 'Pikachu', height: 4, types: ['Electric'], 
+                    abilities: ['static', 'lighting-rod']},
+                   {name: 'Abra', height: 9, types: ['Psychic'], 
+                    abilities: ['synchronize', 'inner-focus']}
+];
