0ebb40bdcd531cc309b85d4a161022a033d78a23
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-10-18 14:34:51 -0700

    Update scripts.js
    Formated

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
-let pokemonList = [{name: 'Charizard', height: 17, types: ['Fire'], 
-                    abilities: ['blaze', 'solar-power']},
-                   {name: 'Pikachu', height: 4, types: ['Electric'], 
-                    abilities: ['static', 'lighting-rod']},
-                   {name: 'Abra', height: 9, types: ['Psychic'], 
-                    abilities: ['synchronize', 'inner-focus']}
+let pokemonList = [{
+    name: 'Charizard', height: 17, types: ['Fire'], 
+    abilities: ['blaze', 'solar-power']
+},
+{
+    name: 'Pikachu', height: 4, types: ['Electric'], 
+    abilities: ['static', 'lighting-rod']
+},
+{
+    name: 'Abra', height: 9, types: ['Psychic'], 
+    abilities: ['synchronize', 'inner-focus']
+}
 ];
