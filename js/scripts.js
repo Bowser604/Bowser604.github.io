cf9afa2f15ef495ea6ce2e9700a3b93db7ddbed9
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-10-18 15:27:52 -0700

    Update scripts.js
    comment height > 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 let pokemonList = [{
-    name: 'Charizard', height: 17, types: ['Fire'], 
+    name: 'Charizard', height: 1.7, types: ['Fire'], 
     abilities: ['blaze', 'solar-power']
 },
 {
-    name: 'Pikachu', height: 4, types: ['Electric'], 
+    name: 'Pikachu', height: 0.4, types: ['Electric'], 
     abilities: ['static', 'lighting-rod']
 },
 {
-    name: 'Abra', height: 9, types: ['Psychic'], 
+    name: 'Abra', height: 0.9, types: ['Psychic'], 
     abilities: ['synchronize', 'inner-focus']
 }
 ];
+
+for (let i = 0; i < pokemonList.height; i++) {
+    if (pokemonList[i].height > 1)
+    {document.write(pokemonList[i].name = " " + "(height:"+ " " + pokemonList [i].height + " ) " + "-Wow. That's big!"+"<br>");
+    }     //pokemon with height >1
