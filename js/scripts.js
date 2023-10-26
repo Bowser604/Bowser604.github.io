71fded7e6cd40fca30b648f293da3fc29e59644b
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-10-25 20:51:25 -0700

    Update scripts.js
    = changed to +

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,8 @@ let pokemonList = [{
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].length > 1)
-    {document.write(pokemonList[i].name = " " + "(height:"+ " " + pokemonList[i].height + " ) " + "-Wow. That's big!"+"<br>"); //condition for pokemon with the height > 1
-   } else {
+    if (pokemonList[i].height > 1)
+    {document.write(pokemonList[i].name + " " + "(height:"+ " " + pokemonList[i].height + " ) " + "-Wow. That's big!"+"<br>"); //condition for pokemon with the height > 1
+} else {
     document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "<br>");}
-}
+    }
