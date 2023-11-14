ada0dc245c3627a3cfabbf2f1b116e0ec2fc2084
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-11-14 08:05:44 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,41 @@
 let pokemonRepository = (function () {
-  let pokemonList = [{
-    name: 'Charizard', height: 1.7, types: ['Fire'], 
-    abilities: ['blaze', 'solar-power']
-},
-{
-    name: 'Pikachu', height: 0.4, types: ['Electric'], 
-    abilities: ['static', 'lighting-rod']
-},
-{
-    name: 'Abra', height: 0.9, types: ['Psychic'], 
-    abilities: ['synchronize', 'inner-focus']
-}
-];
+    let pokemonList = [{
+            name: 'Charizard',
+            height: 1.7,
+            types: ['Fire'],
+            abilities: ['blaze', 'solar-power']
+        },
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            types: ['Electric'],
+            abilities: ['static', 'lighting-rod']
+        },
+        {
+            name: 'Abra',
+            height: 0.9,
+            types: ['Psychic'],
+            abilities: ['synchronize', 'inner-focus']
+        }
+    ];
+
     return {
-        add: function(pokemon) {
+        add: function (pokemon) {
             pokemonList.push(pokemon);
-        };
-        getAll:function(){
-        return pokemonList
-    }
- };
+        },
+        getAll: function () {
+            return pokemonList;
+        }
+    };
 })();
 
 // Using forEach to iterate over the pokemonList from the pokemonRepository
 pokemonRepository.getAll().forEach((pokemon) => {
-  if (pokemon.height > 1) {
-    document.write(
-        ${pokemon.name} (height: ${pokemon.height}) - Wow. Thants Big!<br>
-    );
-  } else {
-    document.write($pokemon.name} (height; ${pokemon.height})<br>);
-  }
+    if (pokemon.height > 1) {
+        document.write(
+            `${pokemon.name} (height: ${pokemon.height}) - Wow. That's Big!<br>`
+        );
+    } else {
+        document.write(`${pokemon.name} (height: ${pokemon.height})<br>`);
+    }
 });
