b21781c82d23554a69864931ae72ff42a87debcb
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-11-28 23:16:08 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,86 @@
-  let pokemonRepository = (function () {
-    let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector(".modal-container");
 
-    // modal container
-  function showMadal() {
-    let modalContainer = document.querySelector(".modal-container");
-    modalContainer.classList.add('is-visible');
+    
+  // modal container
+function showMadal(title, text) {
+  modalContainer.innerHTML = '';
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
+
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = title;
+
+  let contentElement = document.createElement('p');
+  contentElement.innerText = text;
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modalContainer.appendChild(modal);
+    
+  modalContainer.classList.add('is-visible');
+ }
+
+function hideModal() {
+  modalContainer.classList.remove('is-visible');
+ }
+
+function showDialog(title, text) {
+  showModal(title, text);
+
+  // We have defined modalContainer here
+  let modalContainer = document.querySelector('#modal-container');
+
+  // We want to add a confirm and cancel button to the modal
+  let modal = modalContainer.querySelector('.modal');
+
+  let confirmButton = document.createElement('button');
+  confirmButton.classList.add('modal-confirm');
+  confirmButton.innerText = 'Confirm';
+
+  let cancelButton = document.createElement('button');
+  cancelButton.classList.add('modal-cancel');
+  cancelButton.innerText = 'Cancel';
+
+  modal.appendChild(confirmButton);
+  modal.appendChild(cancelButton);
+
+  // We want to focus the confirmButton so that the user can simply press Enter
+  confirmButton.focus();
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();  
   }
+});
+
+modalContainer.addEventListener('click', (e) => {
+  // Since this is also triggered when clicking INSIDE the modal
+  // We only want to close if the user clicks directly on the overlay
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
+  
   document.querySelecter('#show-modal').addEventListener('click',()=> {
-      showModal();
+      showModal('Modal title', 'This is the modal content!');
   });
+
+})();
+
     
   function getAll() {
-        return pokemonList;
+    return pokemonList;
     }
     function addListItem(pokemon){
       let pokemonListElement = document.querySelector(".pokemon-list");
@@ -24,7 +92,7 @@
       pokemonListElement.appendChild(listPokemon); // fixed typo
       button.addEventListener("click", function (event) {   // click event handling logic 
         showDetails(pokemon) //  Updated to show datails 
-    });
+     });
   }
 
   function loadList() {
