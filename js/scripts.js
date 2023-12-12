942b4c9a7ddef17a1b178f4da8d8caf2144de6b8
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2023-12-12 11:43:41 -0800

    Update scripts.js
    1.10 correcting errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
- let pokemonRepository = (function () {
+let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   let modalContainer = document.querySelector(".modal-container");
@@ -87,9 +87,42 @@ function loadList() {
 
 
   // modal container
-function showModal(pokemon) {
-  console.log('modal fire', pokemon)
-  modalContainer.innerHTML = '';
+function showModal(item) {
+  let modalBody = $(".modal-body");
+  let modalTitle = $(".modal-title");
+  let modalHeader = $(".modal-header");
+  // let $modalContainer = $("modal-container");
+  // clear existing content of the modal
+  // modalHeader.empty();
+  modalTitle.empty();
+  modalBody.empty();
+
+  // creating element for name in modal content
+  let nameElement = $("<h>" + item.name + "</h1>");
+  // creating img in madal content
+  let imageElementFront = $('<img class="modal-img"style="width:50%">');
+  imageElementFront.attr("src", item.imageUrlFront);
+  let imageElementBack = $('<img class="modal-img"style="width:50%">');
+  imageElementBack.attr("src", item.imageUrlBack);
+  // creating element for height in madal content
+  let heightElement = $("<p>" + "height : " + item.height + "</p>");
+  //creating element for weight in modal content
+  let weightElement = $("<p>" + "weight : " + item.weight + "</p>");
+  // creating element for type in madal content
+  let typesElement = $("<p>" + "types : " + item.types + "</p>");
+  // creating element for abilities in madal content
+  let abilitiesElement = $("<p>" + "abilities : " + item.abilities + "</p>");
+
+  modalTitle.append(nameElement);
+  modalBody.append(imageElementFront);
+  modalBody.append(imageElementBack);
+  modalBody.append(heightElement);
+  modalBody.append(weightElement);
+  modalBody.append(typesElement);
+  modalBody.append(abilitiesElement);
+}
+  // console.log('modal fire', pokemon)
+  // modalContainer.innerHTML = '';
   let modal = document.createElement('div');
   modal.classList.add('modal');
 
@@ -99,27 +132,38 @@ function showModal(pokemon) {
   closeButtonElement.addEventListener('click', hideModal);
 
   let titleElement = document.createElement('h1');
-  titleElement.innerText = 'Pokemon name' + ': ' + pokemon.name;
+  titleElement.innerText = 'Pokemon name' + ': ' + item.name;
 
   let contentElement = document.createElement('p');
-  contentElement.innerText = 'Pokemon height' + ': ' + pokemon.height;
+  contentElement.innerText = 'Pokemon height' + ': ' + item.height;
 
   let myImage = document.createElement('img');
-  myImage.src = pokemon.imageUrl; 
+  myImage.src = item.imageUrl;
+  
+  // Append elements to the modal
   modal.appendChild(myImage);
-
   modal.appendChild(closeButtonElement);
   modal.appendChild(titleElement);
   modal.appendChild(contentElement);
+
+  // Append modal to the modal container
   modalContainer.appendChild(modal);
 
+  // Add the 'is-visible' class to show the modal
   modalContainer.classList.add('is-visible');
  }
- function hideModal() {
+
+function hideModal() {
   modalContainer.classList.remove('is-visible');
 }
 
-
+// Pass 'item' as an argument to showDetails function
+function showDetails(item) {
+  pokemonRepository.loadDetails(item).then(function () {
+    console.log('show details ', item);
+    showModal(item);
+  });
+}
 
 window.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
@@ -137,10 +181,6 @@ modalContainer.addEventListener('click', (e) => {
 });
 
 
-
-
-
-
   return {
     addListItem: addListItem,
     getAll: getAll,
@@ -148,7 +188,6 @@ modalContainer.addEventListener('click', (e) => {
     loadDetails: loadDetails,
     showDetails: showDetails,
   };
-})();
 
 pokemonRepository.loadList().then(function() {
   pokemonRepository.getAll().forEach(function (pokemon) {
